85232e3a0310190012dc133778da67eb7e1475c4
Elise-Le-Roux <elise.lrdb@gmail.com>
2023-01-03 16:18:23 +0100

    delete bbduser

--- web-app/back/BDDuser.py
+++ web-app/back/BDDuser.py
@@ -22,7 +22,7 @@ def get_images():
         # CouchDB
         couch = couchdb.Server("http://user:user@localhost:5984")
         db = couch['images']
-
+        
         id_images = [
                 "000003.jpg",
                 "000004.jpg",
@@ -31,7 +31,7 @@ def get_images():
 
         images_data = []
         for id in id_images:
-            images_data.append(base64.b64encode(db.get_attachment(id, list(db.get(id) ['_attachments'].keys())).read()).decode('utf-8')) """
+            images_data.append(base64.b64encode(db.get_attachment(id, list(db.get(id) ['_attachments'].keys())).read()).decode('utf-8'))
 
 
         response = {
@@ -41,7 +41,7 @@ def get_images():
             "answer": {"images": [images]},
             "confirm": True
         }
-    
+
     except Exception as e:
         response = {
             "title": "AnswerSrV",
@@ -55,7 +55,9 @@ def get_images():
     finally :
         return jsonify(response)
 
-# Get images uploadees
+
+# Upload Images
+## TO DO !
 @app.route("/upload", methods=["POST"])
 def upload_images():
     try :
@@ -67,16 +69,60 @@ def upload_images():
             "title": "ConfirmSrV",
             "user": req['user'],
             "id_partie": req['id_partie'],
-            "images": req['images']
+            "images": req['images'],
             "confirm": True
         }
     
     except Exception as e:
         response = {
-            "title": "ConfirmSrV",
+            "title": "AnswerSrV",
             "user": req['user'],
             "id_partie": req['id_partie'],
-            "images": req['images']
+            "images": req['images'],
+            "confirm": False,
+            "error": repr(e)
+        }
+        
+    finally :
+        return jsonify(response)
+
+
+# Delete Images associated with a party
+@app.route("/delete", methods=["DELETE"])
+def delete_images():
+    try :
+        # Get JSON request
+        req = request.get_json()
+
+        # CouchDB
+        couch = couchdb.Server("http://user:user@localhost:5984")
+        db = couch['images']
+
+        id_images = [
+                "test"
+        ]
+
+        for id in id_images:
+            # Mango query
+            mango = ({  'selector': {'_id': id},
+                        'fields': ['_id']})
+            query_result = list(db.find(mango))
+            del db[query_result[0].id]
+
+        response = {
+            "title": "AnswerSrV",
+            "user": req['user'],
+            "id_partie": req['id_partie'],
+            "confirm": True
+        }                      
+
+    
+    except Exception as e:
+        print(e)
+        response = {
+            "title": "AnswerSrV",
+            "user": req['user'],
+            "id_partie": req['id_partie'],
             "confirm": False,
             "error": repr(e)
         }
